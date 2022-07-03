d5bf39b8cddf90a5d8df0a657b3bd52bc9f6f4ab
ak0007-code <asahi0107wave@gmail.com>
2022-07-03 03:33:18 +0000

    条件クリアに上り順位を追加

--- auto_predict.php
+++ auto_predict.php
@@ -403,9 +403,121 @@ for($i=0;$i<count($race_results);$i++){
             display:block;
             transition:.2s;
         }
+
+        /* テスト */
+        /* .button {
+            background: lightblue;
+            color: #fff;
+            padding: 0 2em;
+            border: 0;
+            width: 500px;
+            height: 100px;
+            font-size: 45px;
+            border-radius: 5px;
+            font-weight: 900;
+            position: relative;
+            left: 400px;
+            top: 260px;
+            font-family: serif;
+        }
+
+        .button:hover {
+            background: lightcoral;
+            cursor: pointer;
+        }
+
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            height: 100%;
+            width: 100%;
+            overflow: auto;
+            background-color: rgba(0,0,0,0.5);
+        }
+
+        .modal-content {
+            background-color: #f4f4f4;
+            margin: 20% auto;
+            width: 50%;
+            box-shadow: 0 5px 8px 0 rgba(0,0,0,0.2),0 7px 20px 0 rgba(0,0,0,0.17);
+            animation-name: modalopen;
+            animation-duration: 1s;
+        }
+
+        @keyframes modalopen {
+            from {opacity: 0}
+            to {opacity: 1}
+        }
+
+        .modal-header h1 {
+            margin: 1rem 0;
+        }
+
+        .modal-header {
+            background: lightblue;
+            padding: 3px 15px;
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .modalClose {
+            font-size: 2rem;
+        }
+
+        .modalClose:hover {
+            cursor: pointer;
+        }
+
+        .modal-body {
+            padding: 10px 20px;
+            color: black;
+        } */
     </style>
 </head>
 <body>
+
+    <!-- <button id="modalOpen" class="button">Click Me</button>
+    <div id="easyModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="modalClose">×</span>
+            </div>
+            <div class="modal-body">
+            </div>
+        </div>
+    </div>
+    <script>
+        const buttonOpen = document.getElementById('modalOpen');
+        const modal = document.getElementById('easyModal');
+        const buttonClose = document.getElementsByClassName('modalClose')[0];
+
+        //ボタンがクリックされた時
+        buttonOpen.addEventListener('click', modalOpen);
+        function modalOpen() {
+            $('body').css('overflow-y', 'hidden'); 
+            modal.style.display = 'block';
+        };
+
+        //バツ印がクリックされた時
+        buttonClose.addEventListener('click', modalClose);
+        function modalClose() {
+            modal.style.display = 'none';
+            $('body').css('overflow-y','auto'); 
+        };
+
+        //モーダルコンテンツ以外がクリックされた時
+        addEventListener('click', outsideClose);
+        function outsideClose(e) {
+            if (e.target == modal) {
+                modal.style.display = 'none';
+                $('body').css('overflow-y','auto'); 
+            };
+        };
+    </script> -->
+
     <b><?php echo $race_name[$target_num]; ?></b>
     <div class="tatget_scroll">
         <table class="target_table" border="1" style="margin-top: 0%;">
